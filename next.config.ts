fc29a17500ac2d5d0986ec863d7e639d854d591f
Matěj Chamrád <148466838+mtjchm@users.noreply.github.com>
2025-12-13 19:47:31 +0100

    Enable reactStrictMode and set distDir in config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  experimental: {
+  reactStrictMode: true,
+    distDir: 'build',
+    output: 'standalone',  experimental: {
     ppr: true,
   },
   images: {
